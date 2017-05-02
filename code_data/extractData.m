b44823170a691305e5375308fc3b907d5f43ccf4
wwcao <wwcao@colorado.edu>
2017-05-02 08:23:26 -0600

    Chnaged data path

--- code_data/extractData.m
+++ code_data/extractData.m
@@ -7,7 +7,7 @@ for i=2:36
     mat = horzcat(mat,data(:,i*2));
 end
 
-usadata = xlsread('results-20170501-082807.csv','A3:A22');
+usadata = xlsread('DataMiningData.xlsx','A3:A22');
 normusa = usadata./sum(usadata);
 conflictmatrix = mat(:,1:18);
 nonconflictmatrix = mat(:,19:36);
